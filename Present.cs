395d45d17ecddb5d7e62c5aa26e1f5209da1c6c4
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-18 22:39:12 +0530

    The programe with object of Employee wage

---
+++ Present.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectofEmployeewage
+{
+    internal class Present
+    {
+        public  void solution()
+        {
+            Console.WriteLine("Welcome to the Employee Wage");
+            int IS_FULL_TIME = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+
+
+            }
+            Console.ReadLine();
+        }
+
+        }
+}

e8d5b772550947033d9dc32cf5d9d14599d90252
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-18 22:35:51 +0530

    Programe with object

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ObjectofEmployeewage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            Present obj=new Present();
+            obj.solution();
+        }
+    }
+}

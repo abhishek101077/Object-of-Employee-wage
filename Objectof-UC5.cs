dade1dd5378ae5fb374e4454e8434305d428cb48
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-21 20:16:26 +0530

    The fifth file

---
+++ Objectof-UC5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectofEmployeewage
+{
+    internal class Objectof_UC5
+    {
+         public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public void Solution()
+        { 
+
+            Console.WriteLine("Welcome to EmployeeWage Computation");
+            int empHrs = 0;
+            int empWage = 0;
+            int TotalEmpWage = 0;
+            for (int days = 1; days <= NUM_OF_WORKING_DAYS; days++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        // Console.WriteLine("Full Time Employee is Present");
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        //  Console.WriteLine("Part Time Employee is Present");
+                        empHrs = 4;
+                        break;
+                    default:
+                        //   Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
+                        break;
+                }
+                empWage = EMP_RATE_PER_HOUR * empHrs;
+                TotalEmpWage += empWage;
+            }
+            Console.WriteLine("Assumed 20 Working Day in a Month the Employee Wage is : " + TotalEmpWage);
+
+            Console.ReadLine();
+        }
+    
+        }
+}

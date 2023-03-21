bfe2e9ff673011bd05f6a7f9439c8c665006c24a
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-21 20:05:53 +0530

    The second file

---
+++ Objectof-UC-2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectofEmployeewage
+{
+    public class Objectof_UC_2
+    {
+        public void Solution()
+        {
+            Console.WriteLine("Welcome to the Employee Wage Calculation");
+
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+
+
+
+            Console.ReadLine();
+        }
+
+
+        }
+    }

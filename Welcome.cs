cec9bcd129d3402d23d224b4cea5efccb215244e
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-23 18:58:24 +0530

    welcome programme

---
+++ Welcome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectofEmployeewage
+{
+    internal class Welcome
+    { 
+        public void Solution()
+        {
+            Console.WriteLine("Welcome to Employee wage computation");
+            Console.ReadLine();
+        }
+
+    }
+}
